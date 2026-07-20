53ea5f4b19c69ce9bdf5af03408ec6bd56af3d05
imrod1n <a75308475@gmail.com>
2026-07-20 23:53:35 +0300

    Bot can send messages

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-print("Bot starting...")
-
-from dotenv import load_dotenv
+import discord
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.getenv("DISCORD_TOKEN"))
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Бот запущен как {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    text = message.content.lower()
+    if text.startswith("мудрец"):
+        await message.channel.send(f"{message.author.display_name}, друг мой, я не могу ответить на это сообщение. К сожалению, я всё ещё слеп. Я скажу вам, когда прозрею.")
+
+client.run(TOKEN)
