3788a5c699808caeb048359f91ba68b2ff67f4c0
imrod1n <a75308475@gmail.com>
2026-07-21 12:28:26 +0300

    Connected with ai-model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
 
@@ -11,6 +12,21 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+client_ai = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
+
+def ask_ai(prompt):
+    response = client_ai.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=300
+    )
+    return response.choices[0].message.content
+
 @client.event
 async def on_ready():
     print(f'Бот запущен как {client.user}')
@@ -19,9 +35,18 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
+
     text = message.content.lower()
+
     if text.startswith("мудрец"):
-        await message.channel.send(f"{message.author.display_name}, друг мой, я не могу ответить на это сообщение. К сожалению, я всё ещё слеп. Я скажу вам, когда прозрею.")
+        user_text = message.content[7:]
+
+        if not user_text:
+            await message.channel.send("Спроси что-нибудь 🙂")
+            return
+
+        answer = ask_ai(user_text)
+
+        await message.channel.send(answer)
 
 client.run(TOKEN)
